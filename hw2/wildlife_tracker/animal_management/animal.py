ff2bbfcdebed6ad96a3479910f8946bd5f8ffb3c
Vincent Li <vincentli0921@gmail.com>
2024-10-08 01:10:23 -0400

    initial hw2 commit

---
+++ hw2/wildlife_tracker/animal_management/animal.py
@@ -0,0 +1,15 @@
+from typing import Any, Optional
+
+class Animal:
+
+    def __init__(self, animal_id: int, species: str, health_status: Optional[str] = None, age: Optional[int] = None):
+        self.animal_id = animal_id
+        self.species = species
+        self.health_status = health_status
+        self.age = age
+        
+    def get_animal_details(animal_id) -> dict[str, Any]:
+        pass
+    
+    def update_animal_details(animal_id: int, **kwargs: Any) -> None:
+        pass
