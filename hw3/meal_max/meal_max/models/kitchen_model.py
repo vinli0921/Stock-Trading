2a6ddfe1a75a6605e6ed293ac4dcfbf05ed225d0
Vincent Li <vincentli0921@gmail.com>
2024-11-11 18:13:58 -0500

    final updates

--- hw3/meal_max/meal_max/models/kitchen_model.py
+++ hw3/meal_max/meal_max/models/kitchen_model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import logging
+import os
 import sqlite3
 from typing import Any
 
@@ -10,7 +11,6 @@ from meal_max.utils.logger import configure_logger
 logger = logging.getLogger(__name__)
 configure_logger(logger)
 
-# FOR VINCENT
 
 @dataclass
 class Meal:
@@ -66,6 +66,26 @@ def create_meal(meal: str, cuisine: str, price: float, difficulty: str) -> None:
         logger.error("Database error: %s", str(e))
         raise e
 
+def clear_meals() -> None:
+    """
+    Recreates the meals table, effectively deleting all meals.
+
+    Raises:
+        sqlite3.Error: If any database error occurs.
+    """
+    try:
+        with open(os.getenv("SQL_CREATE_TABLE_PATH", "/app/sql/create_meal_table.sql"), "r") as fh:
+            create_table_script = fh.read()
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.executescript(create_table_script)
+            conn.commit()
+
+            logger.info("Meals cleared successfully.")
+
+    except sqlite3.Error as e:
+        logger.error("Database error while clearing meals: %s", str(e))
+        raise e
 
 def delete_meal(meal_id: int) -> None:
     """
--- hw3/meal_max/run_docker.sh
+++ hw3/meal_max/run_docker.sh
@@ -3,7 +3,7 @@
 # Variables
 IMAGE_NAME="meal_max"
 CONTAINER_TAG="0.2.0"
-HOST_PORT=5000
+HOST_PORT=5001
 CONTAINER_PORT=5000
 DB_VOLUME_PATH="./db"   # Adjust this to the desired host path for the database persistence
 BUILD=true  # Set this to true if you want to build the image
@@ -20,6 +20,23 @@ fi
 if [ ! -d "${DB_VOLUME_PATH}" ]; then
   echo "Creating database directory at ${DB_VOLUME_PATH}..."
   mkdir -p ${DB_VOLUME_PATH}
+fi
+
+# Stop and remove the running container if it exists
+if [ "$(docker ps -q -a -f name=${IMAGE_NAME}_container)" ]; then
+    echo "Stopping running container: ${IMAGE_NAME}_container"
+    docker stop ${IMAGE_NAME}_container
+
+    # Check if the stop was successful
+    if [ $? -eq 0 ]; then
+        echo "Removing container: ${IMAGE_NAME}_container"
+        docker rm ${IMAGE_NAME}_container
+    else
+        echo "Failed to stop container: ${IMAGE_NAME}_container"
+        exit 1
+    fi
+else
+    echo "No running container named ${IMAGE_NAME}_container found."
 fi
 
 # Run the Docker container with the necessary ports and volume mappings
